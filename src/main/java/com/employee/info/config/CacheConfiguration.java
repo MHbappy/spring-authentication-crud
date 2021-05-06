01058815638ef9dcc8191a8f227207fa2148543c
BigInt1010 <tofayel.java@gmail.com>
2021-05-06 15:24:19 +0300

    initial commit

--- src/main/java/com/employee/info/config/CacheConfiguration.java
+++ src/main/java/com/employee/info/config/CacheConfiguration.java
@@ -48,6 +48,7 @@ public class CacheConfiguration {
             createCache(cm, com.employee.info.domain.User.class.getName());
             createCache(cm, com.employee.info.domain.Authority.class.getName());
             createCache(cm, com.employee.info.domain.User.class.getName() + ".authorities");
+            createCache(cm, com.employee.info.domain.Employee.class.getName());
             // jhipster-needle-ehcache-add-entry
         };
     }
---
+++ src/main/java/com/employee/info/repository/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.employee.info.repository;
+
+import com.employee.info.domain.Employee;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data  repository for the Employee entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
